6716ba476c9e63fb31090b8bdef6779e182a4989
chris <cmseeling@gmail.com>
2025-03-31 08:48:05 -0500

    room by url param

--- BlazorSignalRApp/BlazorSignalRApp/Hubs/ChatHub.cs
+++ BlazorSignalRApp/BlazorSignalRApp/Hubs/ChatHub.cs
@@ -4,11 +4,12 @@ namespace BlazorSignalRApp.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task JoinRoom(string roomName)
+    public async Task JoinRoom(string roomName, string userName)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
         // Notify the client that they've successfully joined the room
-        await Clients.Caller.SendAsync("ReceiveMessage", "System", $"You have joined the room: {roomName}");
+        //await Clients.Caller.SendAsync("ReceiveMessage", "System", $"You have joined the room: {roomName}");
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", "System", $"{userName} has joined the room.");
     }
 
     public async Task LeaveRoom(string roomName)
