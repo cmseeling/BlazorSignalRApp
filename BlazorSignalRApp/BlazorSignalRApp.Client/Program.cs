f77d61187e8f95a07b46bdc635f1ac75e0d638ca
chris <cmseeling@gmail.com>
2025-04-01 19:43:02 -0500

    add automatic localization

--- BlazorSignalRApp/BlazorSignalRApp.Client/Program.cs
+++ BlazorSignalRApp/BlazorSignalRApp.Client/Program.cs
@@ -1,5 +1,40 @@
+using BlazorSignalRApp.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SoloX.BlazorJsonLocalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddHttpClient(
+    "Host",
+    (sp, c) =>
+    {
+        var hostEnv = sp.GetRequiredService<IWebAssemblyHostEnvironment>();
+        c.BaseAddress = new Uri(hostEnv.BaseAddress);
+    });
+
+builder.Services.AddJsonLocalization(b =>
+{
+    b
+        .AddFallback("Fallback", typeof(_Imports).Assembly)
+        .UseHttpClientJson(options =>
+        {
+            options.ResourcesPath = "Resources";
+            options.NamingPolicy = HttpHostedJsonNamingPolicy;
+            options.ApplicationAssembly = typeof(_Imports).Assembly;
+            options.HttpClientBuilder = sp =>
+            {
+                var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+                return httpClientFactory.CreateClient("Host");
+            };
+        });
+});
+
 await builder.Build().RunAsync();
+
+
+static Uri HttpHostedJsonNamingPolicy(string basePath, string cultureName)
+{
+    return string.IsNullOrEmpty(cultureName)
+        ? new Uri($"{basePath}.json?v=1.0.0", UriKind.Relative)
+        : new Uri($"{basePath}.{cultureName}.json?v=1.0.0", UriKind.Relative);
+}
